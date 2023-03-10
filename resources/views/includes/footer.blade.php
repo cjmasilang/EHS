e9d0dca03df6e355a85e5264b4a245c674f08b30
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2023-03-10 12:34:02 +0800

    Create footer.blade.php

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<footer id="footer" class="footer">
+
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>VBI</span></strong>. All Rights Reserved
+      </div>
+
+    </div>
+  </footer>
